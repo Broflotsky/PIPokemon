fc0ea2266a583c5eebfc25058e617e9fecd6b27a
Broflotsky <ivancho0@hotmail.com>
2022-04-16 14:09:48 -0500

    DB & 2 routes done

---
+++ api/src/routes/helpers.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+const axios = require('axios');
+const { Pokemon , Tipo } =  require ('../db');
+
+
+
+
+module.exports = {
+    
+}
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,104 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const axios = require('axios');
+const { Pokemon , Tipo } =  require ('../db');
 
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+const getInfoApi = async () => {
+        //setting an array in order to be ablee to save all the url from the api
+        const poke_res = (await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=50`)).data.results
+        let info = [];
+        for (let pk of poke_res) {
+            info.push(axios.get(pk.url))
+        }
+
+        //since we are calling the urls into the array as a fetch we are calling all the porimises
+        //we need to use .then in order to get the info
+        return Promise.all(info).then((response) => {
+            //we are get in to the every single pokemon and calling the data that is contains
+            const pokemones = response.map((details) => {
+                //we are retriving and OBJ
+                return (poke = {
+                    name: details.data.name,
+                    id: details.data.id,
+                    img: details.data.sprites.other.home.front_default,
+                    types: details.data.types.map((e) => e.type.name),
+                    attack: details.data.stats[1].base_stat,
+                    height: details.data.height,
+                    weight: details.data.weight,
+                    hp: details.data.stats[0].base_stat,
+                    defense: details.data.stats[2].base_stat,
+                    speed: details.data.stats[5].base_stat,
+                });
+            });
+            return pokemones;
+         // console.log(pokemones);
+        });
+
+}
+
+const getInfoDb = async () => {
+    return await Pokemon.findAll({
+        include:{
+            model: Tipo,
+            attributes: ['name'],
+            through: {
+                attributes: [],
+            },
+        }
+    })
+}
+
+const getAllInfo = async () => {
+    const apiInfo = await getInfoApi();
+    const dbInfo = await getInfoDb();
+    const AllInfo = apiInfo.concat(dbInfo);
+    return AllInfo;
+}
+
+
+router.get('/pokemons', async (req,res) => {      //RUTA /POKEMONS y NAME
+    let name = req.query.name
+    const getPokes = await getAllInfo();
+    if(name){
+        let pokename = await getPokes.filter(e => e.name.toLowerCase().includes(name.toLowerCase()))
+        pokename.length ?
+        res.status(200).send(pokename) :
+        res.status(404).send('No se encontro el Pokémon buscado')
+    } else {
+        res.status(200).send(getPokes)
+    }
+});
+
+router.get('/types', async (req, res)=>{
+    const tiposApi = await getInfoApi()
+    const tipos = tiposApi.map(e => e.types).flat()
+    // console.log(tipos);
+    const save = []
+
+
+    // ESTE FOREACH LO HICE DE FORMA REBUNDANTE PORQUE EL FINDORCREATE DIRECTO SOBRE 'TIPOS' SEGUIA AGREGANDO ITEMS REPETIDOS.
+    const tipos2 = tipos.forEach(e => {
+        if(!save.includes(e)){
+            save.push(e);
+        }
+    })
+  
+    save.forEach(e => {
+            Tipo.findOrCreate({
+            where: { name: e }
+        })
+    })
+  
+    const allTipos = await Tipo.findAll();
+    res.send(allTipos);
+  });
 
 
 module.exports = router;
